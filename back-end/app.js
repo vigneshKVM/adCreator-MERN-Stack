edce19caa495f63c90e132dbe9f221499e4b007e
vigneshKVM <61346787+vigneshKVM@users.noreply.github.com>
2021-02-11 14:53:48 +0530

    UPDATED details

--- back-end/app.js
+++ back-end/app.js
@@ -12,7 +12,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://Vignesh:Vikky1994@cluster0.j97sa.mongodb.net/adCreator';
+  'USE-YOUR-MONGODB-URI';
 
 const app = express();
 const store = new MongoDBStore({
